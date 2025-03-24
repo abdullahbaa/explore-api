0bc33ee12a445a5675e62fab88953720013248ca
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2025-03-24 14:08:48 +0600

    fetch commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Inside index.js');
+// console.log('Inside index.js');func
+function loadUsers2(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(result => displayUsers2(result));
+}
+function displayUsers2(result){
+    const ul = document.getElementById('users-list');
+   for ( const user of result){
+    console.log(user.name);
+    const li = document.createElement('li');
+    li.innerText = user.name;
+    ul.appendChild(li);
+   }
+}
